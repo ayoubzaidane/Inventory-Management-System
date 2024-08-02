c91ea573cfea944646f4d9952a7b60880b0da71d
ayoubzaidane <ayoubcode987@gmail.com>
2024-08-02 01:45:15 +0100

    day2

--- inv-mgmt/src/app/app.component.ts
+++ inv-mgmt/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: [
+    './app.component.css',   // Include your CSS file
+    './app.component.scss'  // Include your SCSS file
+  ]
 })
 export class AppComponent {
   title = 'inv-mgmt';
 }
+
+
